96c43df2487992782f4e2fbfa37215d80e9ad0e8
Josh Klag <joshua.klag@vanderbilt.edu>
2016-11-28 13:05:41 -0600

    added rest of project

---
+++ Assets/Controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Controller : NetworkBehaviour {
+
+	public GameObject camera;
+	public GameObject gvrPrefab;
+//	public GameObject reticle;
+//	public GameObject eventSystem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+
+	public override void OnStartLocalPlayer()
+	{
+		print ("called");
+		foreach(SkinnedMeshRenderer s in GetComponentsInChildren<SkinnedMeshRenderer>()){
+				s.material.color = Color.blue;
+		}
+		Instantiate (camera);
+		Instantiate (gvrPrefab);
+		/*GameObject cam = new GameObject();
+		cam.AddComponent<Camera>();
+		cam.tag = "MainCamera";
+		cam.name = "cam";
+		cam.transform.parent = gameObject.transform;
+		cam.transform.localPosition = new Vector3 (0, 2, -5);
+		cam.transform.localRotation = Quaternion.Euler (20, 0, 0);*/
+	}
+}
+
