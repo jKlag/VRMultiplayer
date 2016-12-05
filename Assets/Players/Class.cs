d33373a8bb9eb89baefdb96b47895a81718251d4
csx283vu <turnerstrayhorn@Turners-MacBook-Pro.local>
2016-12-04 23:46:42 -0600

    Classes yo. Fixed running problem

---
+++ Assets/Players/Class.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Class : NetworkBehaviour {
+
+	public GameObject warriorItem;
+	public GameObject detectiveItem;
+	public GameObject destroyerItem;
+
+	private string playerClass;
+
+	Color warriorColor = Color.blue;
+	Color detectiveColor = Color.yellow;
+	Color destroyerColor = Color.red;
+	int classNumber;
+
+	[SyncVar]
+	public Color m_color;
+
+
+
+	public string getClass(){
+		return playerClass;
+	}
+
+	public void setClass(int classNum){
+		print ("Called");
+		classNumber = classNum;
+		switch (classNumber) {
+		case 1:
+			setDetective ();
+			break;
+		case 2:
+			setDestroyer ();
+			break;
+		case 3:
+			setWarrior ();
+			break;
+		}
+	}
+
+	public int getClassNum(){
+		return classNumber;
+	}
+
+	void setWarrior(){
+		playerClass = "warrior";
+		SetColor (warriorColor);
+		placeItem (warriorItem);
+
+	}
+	void setDetective(){
+		playerClass = "detective";
+		SetColor (detectiveColor);
+		placeItem (detectiveItem);
+
+		//TODO:Dynamically attach warrior class
+	}
+	void setDestroyer(){
+		playerClass = "destroyer";
+		SetColor (destroyerColor);
+		placeItem (destroyerItem);
+		//TODO:Dynamically attach warrior class
+
+	}
+
+	void SetColor(Color c){
+		CmdSetMeshColors (c);
+	}
+
+	void placeItem(GameObject theItem){
+		GameObject emptyItem = GameObject.FindGameObjectWithTag("playerItem");
+		GameObject item = Instantiate (theItem);
+		item.transform.position = emptyItem.transform.position;
+		item.transform.rotation = emptyItem.transform.rotation;
+		item.transform.Rotate (new Vector3 (0, 90, 0));
+		item.transform.parent = emptyItem.transform.parent;
+		CmdSpawnItem (item);
+	}
+
+	[Command]
+	public void CmdSpawnItem(GameObject item){
+		NetworkServer.Spawn (item);
+
+	}
+
+
+	//Server Commands
+	[Command]
+	public void CmdSetMeshColors(Color c)
+	{
+		m_color = c;
+		foreach (SkinnedMeshRenderer mesh in gameObject.GetComponentsInChildren<SkinnedMeshRenderer>())
+		{
+			mesh.material.color = m_color;
+		}
+		RpcSetColor (c);
+
+	}
+
+	[ClientRpc]
+	void RpcSetColor( Color c )
+	{
+		m_color = c;
+		foreach (SkinnedMeshRenderer mesh in gameObject.GetComponentsInChildren<SkinnedMeshRenderer>())
+		{
+			mesh.material.color = m_color;
+		}
+	}
+
+
+
+	public void attack(RaycastHit hit){
+		print ("attacking " + hit.collider.name);
+		//TODO: Logic for classes attacking different colliders
+	}
+
+
+}
---
+++ Assets/Players/PlayerController.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	GameObject player;
+
+	private NavMeshAgent navMeshAgent;
+
+	private Ray ray;
+
+	bool hasKey = false;
+
+	enum PClass {hammer,sword,magnifier};
+
+	PClass mClass = PClass.sword;
+
+	public float attackRate;
+	private float nextAttack;
+
+	public float shoutRate;
+	private float nextShout;
+
+	Animator animator;
+	NetworkAnimator netAnim;
+
+	private bool walking;
+
+	//public GameObject markerObject;
+	//GameObject marker;
+
+	public GameObject swordCursor;
+	public GameObject gvrCursor;
+
+	public GameObject keyIcon;
+	bool monsterDying = false;
+
+
+
+	//Sets the localplayer
+	public void setPlayer(GameObject localPlayer){
+		player = localPlayer;
+		netAnim = player.GetComponent<NetworkAnimator> ();
+		animator = player.GetComponent<Animator> ();
+		navMeshAgent = player.GetComponent<NavMeshAgent> ();
+		transform.position = player.transform.position + new Vector3(0,1.2f,0) + .3f*player.transform.forward;
+		float yRot = transform.rotation.eulerAngles.y;
+		player.transform.eulerAngles = new Vector3 (0, yRot, 0);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//make sure player is lp. may not be nec
+		if (!player.GetComponent<Controller> ().getIsLocalPlayer ()) {
+			return;
+		}
+
+
+		animator.SetFloat ("Forward", Vector3.Magnitude (navMeshAgent.velocity), 0.1f, Time.deltaTime);
+
+
+		float yRot = transform.rotation.eulerAngles.y;
+		//transform player position with camera position
+		player.transform.eulerAngles = new Vector3 (0, yRot, 0);
+		transform.position = player.transform.position + new Vector3 (0, 1.2f, 0) + .3f * player.transform.forward;
+
+
+		//on click
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			ray.origin = transform.position;
+			ray.direction = transform.forward;
+
+			RaycastHit hit;
+
+
+			if (Physics.Raycast (ray, out hit, 50)) {
+				if (hit.collider.gameObject.name == "Plane") {
+					navMeshAgent.SetDestination (hit.point);
+					navMeshAgent.Resume ();
+				} else {
+					attack (hit);
+				}
+			} else {
+				netAnim.SetTrigger ("Attack");
+			}
+		}
+
+
+	
+		
+	}
+
+	//plays attack animation and calls attack method in class
+	void attack(RaycastHit hit){
+		netAnim.SetTrigger ("Attack");
+		nextAttack = Time.time + attackRate;
+		player.GetComponent<Class> ().attack (hit);
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "key") {
+			hasKey = true;
+			Destroy (col.gameObject);
+			keyIcon.transform.localScale = new Vector3 (.2f, .2f, .2f);
+		}
+		if (col.gameObject.tag == "fakekey") {
+			if (mClass != PClass.magnifier) {
+				hasKey = true;
+				keyIcon.transform.localScale = new Vector3 (.2f, .2f, .2f);
+			} 
+			Destroy (col.gameObject);
+		}
+	}
+		
+
+}
